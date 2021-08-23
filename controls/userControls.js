d03c6825a721938f615c1a7eafb85298ee1e47dc
Jude Okagu <juud0010@gmail.com>
2021-08-22 23:33:37 -0400

    signup&signin done and tested with postman

--- controls/userControls.js
+++ controls/userControls.js
@@ -1,58 +1,43 @@
 const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
-const asyncHandler = require('express-async-handler');
 
-exports.signup = asyncHandler(async (req, res) => {
-  const { username, email, password } = req.body;
+exports.signup = async (req, res) => {
+  try {
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-  const userExists = await User.findOne({ email });
-
-  if (userExists) {
-    res.status(400);
-    throw new Error('User already exists');
-  }
-
-  const hashedPassword = await bcrypt.hash(password, 20);
-
-  const user = await User.create({
-    username,
-    email,
-    password: hashedPassword,
-  });
-
-  if (user) {
-    res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
+    //create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
     });
-  } else {
-    res.status(400);
-    throw new Error('Invalid user data');
+
+    //save user and respond
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
   }
-});
+};
 
 exports.signin = async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json('user not found');
 
-  const user = await User.findOne({ email });
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json('wrong password');
 
-  if (!user) {
-    res.status(400);
-    req.session.error = 'User does not exsist';
-    throw new Error('User does not exsist');
+    req.session.username = user.username;
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
   }
-
-  const match = await bcrypt.compare(password, user.password);
-
-  if (!match) {
-    res.status(400);
-    req.session.error = 'User credentials Invalid';
-    throw new Error('User credentials Invalid');
-  }
-
-  req.session.username = user.username;
-  res.status(200);
 };
 
 exports.test = (req, res) => {
--- models/userModel.js
+++ models/userModel.js
@@ -4,13 +4,13 @@ const UserSchema = new mongoose.Schema(
   {
     email: {
       type: String,
-      trim: true,
       required: true,
       unique: true,
     },
     password: {
       type: String,
       required: true,
+      min: 6,
     },
   },
   { timestamps: true }
